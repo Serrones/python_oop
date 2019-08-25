7861cc8769baf85a73e713ec0299780d74c497d4
Serrones <fabioserrones@gmail.com>
2019-08-25 13:34:03 -0300

    Alter policy in runtime

--- program_hr.py
+++ program_hr.py
@@ -1,4 +1,4 @@
-from hr import PayrollSystem
+from hr import PayrollSystem, HourlyPolicy
 from productivity import ProductivitySystem
 from employees import EmployeeDatabase
 
@@ -6,5 +6,7 @@ productivity_system = ProductivitySystem()
 payroll_system = PayrollSystem()
 employee_database = EmployeeDatabase()
 employees = employee_database.employees
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
 productivity_system.track(employees, 40)
 payroll_system.calculate_payroll(employees)
